14c8dbafe1f96ea38a97eeedbba9bdde80317702
Ibrahim Yusuf <ibrobk@gmail.com>
2024-05-04 00:09:56 +0100

    enter search added

--- index.js
+++ index.js
@@ -2,25 +2,43 @@ var btn = document.getElementById('btn');
 var result =  document.getElementById('result');
 
 
-btn.addEventListener('click', ()=>{
-    let username = document.getElementById('in').value;
-    let url = "https://api.genderize.io?name="+username;
+let gender = {
+    // username: document.getElementById('in').value,
+    // url: "https://api.genderize.io?name="+username,
+    getData: function(username){
+        var url = "https://api.genderize.io?name="+username;
+        fetch(url)
+        .then((res)=>res.json())
+        .then((data)=>this.displayData(data));
+    },
 
 
-    let gender = {
-        
-        getData: function(){
-            fetch(url)
-            .then((res)=>res.json())
-            .then((data)=>this.displayData(data));
-        },
-
-
-        displayData: (data)=>{
-            // const {name, gender} = data;
-           result.innerHTML = data.name+" your gender is "+data.gender;
-        }
+    displayData: (data)=>{
+        // const {name, gender} = data;
+       result.innerHTML = data.name+" your gender is "+data.gender;
     }
+}
 
-    return gender.getData();
-})
+
+btn.addEventListener('click', function(){
+   var username = document.getElementById('in').value;
+//     var url = "https://api.genderize.io?name="+username;
+
+
+        return gender.getData(username);
+}
+);
+
+
+
+document.getElementById("in").addEventListener('keyup', function(event){
+    event.preventDefault;
+    if(event.key == "Enter"){
+        var username = document.getElementById('in').value;
+
+      gender.getData(username);
+
+
+    }
+    
+});
